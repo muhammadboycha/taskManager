e94ef2ab7311bb79cc61ff69b8a19f4eb4a4acd8
muhammadboycha <muhammadboycha@gmail.com>
2024-11-16 16:50:50 +0530

    Style added

--- src/component/TaskList.js
+++ src/component/TaskList.js
@@ -2,15 +2,37 @@ import React, { useContext } from "react";
 import { TaskContext } from "../TaskContext";
 export const TaskList = () => {
   const { tasks } = useContext(TaskContext);
+  const styles = {
+    container: {
+      height: "100hv",
+      width: "100%",
 
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    listContainer: {
+      padding: "10px",
+      width: "30%",
+      borderRadius: "15px",
+      backgroundColor: "gray",
+    },
+    listItem: {
+      backgroundColor: "lightgray",
+      padding: "10px",
+      borderRadius: "15px",
+    },
+  };
   return (
-    <div>
-      <h2>Task List</h2>
-      <ul>
+    <div style={styles.container}>
+      <div style={styles.listContainer}>
+        <h2>Task List</h2>
         {tasks.map((task, index) => (
-          <li key={index}>{task.taskTitle}</li>
+          <p style={styles.listItem} key={index}>
+            {task.taskTitle}
+          </p>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
